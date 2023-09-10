ef63a66bfd282bda5b3246e99f75a8837d8f707b
Moi <moi@nous-deb.home>
2023-09-10 15:20:26 +0200

    Initial push in github repository

---
+++ Contents/mods/BarricadeHurtZombies/media/lua/server/BHZcore.lua
@@ -0,0 +1,54 @@
+local PlayerBuiltAndMovedObject = function(thump_target) -- Return true if ThumpableObject is PlayerBuilt or Moved Object
+	if instanceof(thump_target, "IsoThumpable") then return true -- Pass if playerbuilt
+	elseif instanceof(thump_target, "IsoBarricade") then return true-- Pass if barricade on window frame with broken/without window
+	elseif instanceof(thump_target, "BarricadeAble") and not instanceof(thump_target, "IsoThumpable") then 
+		if not thump_target:isBarricaded() then return false-- Pass if barricaded non-player built object
+		else return true
+		end
+	elseif not instanceof(thump_target, "IsoThumpable") then return false-- Cancel other non-playerbuilt
+	end
+end
+
+local All = function(thump_target)
+	return true
+end
+
+local Nothing = function(thump_target)
+	return false
+end
+
+local function GetHurtingBarricade(param1) -- Assign funtion depending on HurtingBarricade option
+	if param1 == 1 then return PlayerBuiltAndMovedObject
+	elseif param1 == 2 then return All
+	else return Nothing
+	end
+end
+
+local GameSpeedToDmgMultiplier = {1,5,20,40}
+local BHZ_THUMP_DMG = SandboxVars.BarricadeHurtZombies.BarricadeDamage / 100
+local BHZ_THUMP_FUNC = GetHurtingBarricade(SandboxVars.BarricadeHurtZombies.HurtingBarricade) 
+
+local function HurtZombieThumping(x, y, z, radius, volume, source)
+	if instanceof(source, "IsoZombie") then
+		if source:getCurrentStateName() == "ThumpState" then
+			local thump_target = source:getThumpTarget()
+			local thump_dmg = BHZ_THUMP_DMG
+			local blood_int = 1
+			if not thump_target then return; end
+			local damage_multiplier = thump_target:getModData().BarricadeDamageMultiplier
+			if damage_multiplier then 
+				thump_dmg = thump_dmg * thump_target:getModData().BarricadeDamageMultiplier
+				blood_int = blood_int * thump_target:getModData().BarricadeDamageMultiplier 
+			else
+				if not BHZ_THUMP_FUNC(thump_target) then return; end
+			end
+	        local new_z_health = source:getHealth() - (thump_dmg * GameSpeedToDmgMultiplier[getGameSpeed()])
+			if new_z_health <= 0 then source:Kill(source:getCell():getFakeZombieForHit(), true) return 
+			end
+			source:setHealth(new_z_health)
+			local sqr = source:getSquare()
+			if sqr then addBloodSplat(sqr, blood_int) end;
+		end
+	end
+end
+Events.OnWorldSound.Add(HurtZombieThumping)
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# ProjectZomboid-BHZ
-BarricadeHurtZombie mod for Project Zomboid game
+## Barricade Hurt Zombies for Project Zomboid Game
+
+This repository contains source code for [Barricade Hurt Zombies mod](https://steamcommunity.com/sharedfiles/filedetails/?id=2913137598) for [Project Zomboid](https://projectzomboid.com/blog/) game made by The Indie Stone.
+
+The mod intends to make Zombies take damage when thumping on objects, it has been created along [Cheval de frise mod](https://steamcommunity.com/sharedfiles/filedetails/?id=2927800433), which add a defensive buildable item in the game.
+
+Logo made on [Free Logo Maker](https://www.freelogodesign.org/).
+
+## License
+
+You are free to include it on modpack for your multiplayer server.
+
+## Contact
+
+Add me on Discord: brightex
